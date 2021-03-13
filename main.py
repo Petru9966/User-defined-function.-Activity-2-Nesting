53e5a4f3c52753ff01c6e70ef96e057dc3eb7f74
Replit user <replituser@example.com>
2021-03-13 08:40:18 +0000

    Simple nested function created and called for 3 times

---
+++ main.py
@@ -0,0 +1,14 @@
+#Create a nested funtion
+def take_decision():
+  print("What lies before us?")
+  answer=input()
+  if(answer=="a large boulder"):
+    print("\n\t\t\tIt's time to run!")
+  else:
+      print("\n\t\t\tWe are safe!")
+take_decision()
+take_decision()
+take_decision()
+print("\n\t\t\tProgram ended!")
+
+    
